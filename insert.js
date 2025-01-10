8563c210ce39b27adf194e8f7f7fdab614db9d2f
Akshay namdev <akshaynamdev963@gmail.com>
2025-01-10 17:18:42 +0530

    data

---
+++ insert.js
@@ -0,0 +1,31 @@
+
+
+
+const mybtn=document.getElementById('save')
+
+mybtn.addEventListener('click',myData)
+
+function myData(){
+
+const name=document.getElementById('name').value
+const id=document.getElementById('id').value
+const post=document.getElementById('post').value
+const salary=document.getElementById('salary').value
+const api="http://localhost:5000/employes"
+
+    fetch(api,{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ 
+
+            "name":name,
+            "id":id,
+            "post":post,
+            "salary":salary
+         })
+    }).then((res)=>{
+        alert("Data save successfully")
+    })
+}
---
+++ script.js
@@ -0,0 +1,33 @@
+const mybtn=document.getElementById('btn')
+
+const h=document.getElementById('show')
+
+mybtn.addEventListener('click',myShow)
+
+async function myShow(){
+
+    let response= await fetch("http://localhost:5000/employes")
+    let mydata= await response.json()
+
+    let table=`<table>
+              <tr>
+              <th>name</th>
+              <th>id</th>
+              <th>post</th>
+              <th>salary</th>
+              
+              </tr>   
+    
+    `
+    mydata.map((e)=>{
+        table+=`<tr>
+                 <td>${e.name}</td>        
+                 <td>${e.id}</td>        
+                 <td>${e.post}</td>        
+                 <td>${e.salary}</td>        
+        
+        </tr>`
+    })
+    table+="</table>"
+    h.innerHTML=table
+}
